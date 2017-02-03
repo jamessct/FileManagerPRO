975073280176b5b8f4a3a2f0ddd633e08b103b96
James Scott <jamessct@hotmail.co.uk>
2017-02-03 17:03:31 +0000

    added logo on welcome screen

--- Program.cs
+++ Program.cs
@@ -8,6 +8,12 @@ namespace ConsoleApplication
         {   
             Console.Title = "File Manager PRO 2.0";
             Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("   ___ _ _                                                     ___  __    ___   ____    ___  ");
+            Console.WriteLine("  / __(_) | ___    /\\/\\   __ _ _ __   __ _  __ _  ___ _ __    / _ \\/__\\  /___\\ |___ \\  / _ \\ ");
+            Console.WriteLine(" / _\\ | | |/ _ \\  /    \\ / _` | '_ \\ / _` |/ _` |/ _ \\ '__|  / /_)/ \\// //  //   __) || | | |");
+            Console.WriteLine("/ /   | | |  __/ / /\\/\\ \\ (_| | | | | (_| | (_| |  __/ |    / ___/ _  \\/ \\_//   / __/ | |_| |");
+            Console.WriteLine("\\/    |_|_|\\___| \\/    \\/\\__,_|_| |_|\\__,_|\\__, |\\___|_|    \\/   \\/ \\_/\\___/   |_____(_)___/ ");
+            Console.WriteLine("                                           |___/                                             ");;
             Console.WriteLine("Welcome to File Manager PRO! To proceed, please select a number from the list below:");
             Console.WriteLine();
             ListMaker list = new ListMaker();
