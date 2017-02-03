1b875d7440b97ad273789912ea5e8a6abe68e483
James Scott <jamessct@hotmail.co.uk>
2017-02-03 17:16:04 +0000

    typo

--- src/Utilities.cs
+++ src/Utilities.cs
@@ -10,21 +10,21 @@ namespace ConsoleApplication
             {
                 double megabytes = (bytes / 1048576f);
                 double rounded = Math.Round(megabytes, 2);
-                string answer = (rounded + "mb");
+                string answer = (rounded + "MB");
                 return answer;
             }
             if (bytes > 1073741824)
             {
                 double gigabytes = (bytes / 1073741824f);
                 double rounded = Math.Round(gigabytes, 2);
-                string answer = (rounded + "gb");
+                string answer = (rounded + "GB");
                 return answer;
             }
             else
             {
                 double kilobytes = (bytes / 1024f);
                 double rounded = Math.Round(kilobytes, 2);
-                string answer = (rounded + "kb");
+                string answer = (rounded + "KB");
                 return answer;
             }
         }
