4b1a1fa2fe010934089fea12a6806dd4e3f2db58
James Scott <jamessct@hotmail.co.uk>
2017-02-07 14:35:05 +0000

    anything to commit?

---
+++ test/TableClassFactTest.cs
@@ -0,0 +1,27 @@
+// using Xunit;
+// using ConsoleApplication;
+
+// namespace MyApp
+// {
+//     public class TableClassFactTest
+//     {
+//         // private readonly ITestOutputHelper output;
+
+//         // public TableClassFactTest(ITestOutputHelper output)
+//         // {
+//         //     this.output = output;
+//         // }
+//         [Fact]
+//         public void CanCreateLine()
+//         {
+//             //Assign
+//             var table = new TableMaker();
+
+//             //Act
+//             var ActualResult = table.PrintLine();
+
+//             //Assert
+//             Assert.Equal(Expected, ActualResult);
+//         }
+//     }
+// }
