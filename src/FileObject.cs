b84937c1196908f329936be7a5b21d023b5f6ba0
James Scott <jamessct@hotmail.co.uk>
2017-02-20 09:20:55 +0000

    yeah

---
+++ src/FileObject.cs
@@ -0,0 +1,23 @@
+// using ExtensionMethods;
+
+// namespace ConsoleApplication
+// {
+//     class File
+//     {
+//         public string Name()
+//         {
+//             string name = path.FileName();
+//             return name;
+//         }
+
+//         public string Size()
+//         {
+//             string size = ;
+//         }
+
+//         public string LastAccess(path)
+//         {
+
+//         }
+//     }
+// }
