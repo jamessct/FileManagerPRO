177521a6d16fec7fa8cb80c4608809276af41637
James Scott <jamessct@hotmail.co.uk>
2017-03-10 17:27:13 +0000

    I added like a line but its the end of the day so

--- test/ListMakerTheoryTest.cs
+++ test/ListMakerTheoryTest.cs
@@ -8,11 +8,13 @@ namespace MyApp
     {
         [Theory]
         [InlineDataAttribute(new string[] {@"c:\Projects\Tests\IndexTests\index.txt", @"c:\Projects\Tests\IndexTests\test1.txt", @"c:\Projects\Tests\IndexTests\test2.txt"}, @"c:\Projects\Tests")]
-        public void CanCreateArrayOfFilesForTable(List<DataObject> fileList, string input)
+        public void CanCreateArrayOfFilesForTable(string[] files, string input)
         {
             //Assign
             ListMaker list = new ListMaker();
+            List<DataObject> fileList = new List<DataObject>();
             int expectedArrayLength = 6;
+            
             //Act
             string[] result = list.CreateTable(fileList);
             int answer = result.Length;
